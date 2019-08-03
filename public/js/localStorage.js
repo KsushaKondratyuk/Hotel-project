5e6a8962eae294dae5849055cad3eea40df73a5c
Ksusha <ksusha123456789@gmail.com>
2019-08-04 00:18:14 +0300

    added sorting

---
+++ public/js/localStorage.js
@@ -0,0 +1,151 @@
+function addToLocalStorage(){
+	key = document.forms["localStorageAddForm"]["key"].value;
+	value = document.forms["localStorageAddForm"]["value"].value;
+	
+	if (localStorage.getItem(key)){
+		document.getElementById("result").innerHTML = "Key already exists, dont added";
+		return false;
+	}
+	
+	localStorage.setItem(key, value);
+	document.getElementById("result").innerHTML = "Success";
+	return false;
+}
+
+function getValueByKey(){
+	key = document.forms["localStorageGetForm"]["key"].value;
+	data = localStorage.getItem(key);
+	if (data === null){
+		document.getElementById("result").innerHTML = "No values by key";
+		return false;
+	}
+	document.getElementById("result").innerHTML = data;
+	return false;
+}
+
+function readJSONToDict() {
+	roomData = JSON.parse(data);
+	return roomData;
+}
+
+function readJSONToArray() {
+	roomData = JSON.parse(data);
+	var items = Object.keys(roomData).map(function(key) {
+		return [key, roomData[key]];
+	  });
+	return items;
+}
+
+function fillAllRooms(roomData) {
+	i = 0;
+	str = "";
+	console.log(roomData);
+	for(let element in roomData) {
+		str += " <li class='rooms-list-item'>";
+		str += " <div class='rooms-inner-container'>";
+		str +=	"<img class='rooms-picture' src=" + roomData[element][1].room_image + " alt='Фото одноместного номера'>";
+		str += " <div class='rooms-description'>";
+		str += "<p class='rooms-name'>"+ roomData[element][1].room_name + "</p>";
+		str += "<div class='rooms-type'>";
+		str += "<p class='general-description'>" + roomData[element][1].room_small_description + "</p>";
+		str += "<p class='rooms-price'>"+ roomData[element][1].room_price + "$" + "</p>";
+		str += "</div>";
+		str += "<div class='rooms-actions'>";
+		str += "<a class='button button-more' href='room.html?id="+roomData[element][0]+"'>Подробнее</a>";
+		str += "<a class='button button-book' href=''>Забронировать</a>";
+		str += "</div>";
+		str += "</div>";
+		str += "</div>";
+		str += "</li>";
+		i += 1;
+	}
+	document.getElementById("rooms").innerHTML = str;
+	document.getElementById("counts").innerHTML = "Найдено: " + i;
+}
+
+function fillAllRoomsNotSorted(){
+	items = readJSONToArray();
+	fillAllRooms(items);
+}
+
+function fillAllRoomsByPriceDesc(){
+	items = readJSONToArray();
+	//sort
+	items.sort(function(first, second) {
+		return second[1].room_price - first[1].room_price;
+	  });
+	
+	fillAllRooms(items);
+}
+
+function fillAllRoomsByPriceAsc(){
+	items = readJSONToArray();
+	//sort
+	items.sort(function(first, second) {
+		return first[1].room_price - second[1].room_price;
+	  });
+	
+	fillAllRooms(items);
+}
+
+function fillAllRoomsByCapabilityDesc(){
+	items = readJSONToArray();
+	//sort
+	items.sort(function(first, second) {
+		return second[1].room_capability - first[1].room_capability;
+	  });
+	
+	fillAllRooms(items);
+}
+
+function fillAllRoomsByCapabilityAsc(){
+	items = readJSONToArray();
+	//sort
+	items.sort(function(first, second) {
+		return first[1].room_capability - second[1].room_capability;
+	  });
+	
+	fillAllRooms(items);
+}
+
+function fillRoom() {
+	// read id from url
+	var url_string = window.location.href;
+	var url = new URL(url_string);
+	var id = url.searchParams.get("id");
+
+	roomData = readJSONToDict();
+	str = "";
+	str += "<h1 class='page-title'>"+ roomData[id].room_name +"</h1>";
+	str += "<div class='room-columns'>";
+	str += "<section class='room-photos'>";
+	str += "<h2 class='visually-hidden'>Фото номера</h2>";
+	str += "<p class='room-photo-full'>";
+	str += "<img src='"+ roomData[id].room_image +"' alt='Фото номера большое'>";
+	str += "</p>";
+	str += "<ul class='room-photo-small'>";
+	roomData[id].room_image_add.forEach(element =>{
+		str += "<li><img src='" + element + "'  alt='Фото номера маленькое'></li>";
+	});
+	str += "</ul>";
+	str += "</section>";
+	str += "<section class='room-information'>";
+	str += "<h2 class='room-information-title'>Описание комнаты</h2>";
+	str += "<p class='room-description'>" + roomData[id].room_big_description + "</p>";
+	str += "<p class='room-book'>";
+	str += "<b class='room-price'>" + roomData[id].room_price + "$</b>";
+	str += "<a href='' class='button'>Забронировать</a>";
+	str += "</p>";
+	str += "<h3>Услуги в даном номере:</h3>";
+	str += "<ul class='room-set'>";
+	roomData[id].room_options.forEach(element => {
+		str += "<li>"+element+"</li>";
+	});
+	
+	str += "</ul>";
+	str += "</section>"
+	str += "</div>"
+
+	document.getElementById("room_info").innerHTML = str;
+}
+
